4552ca0f6e462003f7b99bf3f6c5d70b1676ad02
Nicolas-Orozco <nicolasorozcodavila@gmail.com>
2023-03-25 12:57:15 -0500

    Add solution

--- first-test/fizzbuzz.test.js
+++ first-test/fizzbuzz.test.js
@@ -10,6 +10,13 @@ const fizzbuzz = (number) => {
   if (typeof number !== "number") {
     throw new Error("parameter provided must be a number");
   }
+  if (Number.isNaN(number)) {
+    throw new Error("parameter provided must be a number");
+  }
+  if (number % 15 === 0) return "fizzbuzz";
+  if (number % 3 === 0) return "fizz";
+  if (number % 5 === 0) return "buzz";
+  return number;
 };
 describe("fizzbuzz", () => {
   it("should be a function", () => {
@@ -18,7 +25,12 @@ describe("fizzbuzz", () => {
   it("should throw if not number is provided as parameter", () => {
     expect(() => fizzbuzz()).toThrow();
   });
-  it("should throw a speicific error message if not number is provided as parameter", () => {
+  it("should throw a specific error message if not number is provided as parameter", () => {
     expect(() => fizzbuzz()).toThrow("parameter provided must be a number");
   });
+  it("should throw a specific error message not number is provided", () => {
+    expect(() => fizzbuzz(NaN)).toThrow("parameter provided must be a number");
+  });
 });
+
+console.log(fizzbuzz(7));
